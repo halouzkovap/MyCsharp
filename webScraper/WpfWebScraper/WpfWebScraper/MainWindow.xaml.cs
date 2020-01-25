4641000ab43d46c8584f2184f97261abf8a0a5ff
Petra <pierrett75@gmail.com>
2020-01-25 17:56:09 +0100

    problem there with uploude date to file

---
+++ webScraper/WpfWebScraper/WpfWebScraper/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace WpfWebScraper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Scraper scraper;
+        public MainWindow()
+        {
+            InitializeComponent();
+            scraper = new Scraper();
+            DataContext = scraper;
+
+        }
+
+        private void BtnScraper_Click(object sender, RoutedEventArgs e)
+        {
+            scraper.ScrapeData(TbPage.Text);
+
+        }
+
+        private void ItemExport_Click(object sender, RoutedEventArgs e)
+        {
+            scraper.Export();
+        }
+    }
+}
---
+++ webScraper/WpfWebScraper/WpfWebScraper/Scraper.cs
@@ -0,0 +1,57 @@
+﻿using HtmlAgilityPack;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Web;
+
+
+namespace WpfWebScraper
+{
+	public class Scraper
+	{
+		private ObservableCollection<EntryModel> _entries = new ObservableCollection<EntryModel>();
+
+		public ObservableCollection<EntryModel> Entries
+		{
+			get { return _entries; }
+			set { _entries = value; }
+		}
+
+		public void ScrapeData(string page)
+		{
+			var web = new HtmlWeb();
+			var doc = web.Load(page);
+			var Articles = doc.DocumentNode.SelectNodes("//*[@class = 'article-single']");
+
+			foreach (var article in Articles)
+			{
+				var header = HttpUtility.HtmlDecode(article.SelectSingleNode(".//li[@class = 'article-header']").InnerText);
+				var description = HttpUtility.HtmlDecode(article.SelectSingleNode(".//li[@class = 'article-copy']").InnerText);
+				Debug.Print($"Title: {header}\n Description: {description}");
+
+				_entries.Add(new EntryModel { Title = header, Description = description });
+			}
+		}
+		public void Export()
+		{
+
+
+			//using (TextWriter tw = File.CreateText("SampleData.csv"))
+			using (StreamWriter streamWriter = new StreamWriter("SampleData.csv"))
+			{
+
+				//var cw = new CsvWriter(streamWriter);
+
+				foreach (var entry in Entries)
+				{
+					streamWriter.Write(entry + "; ");
+					//cw.WriteRecord(entry);
+					//cw.NextRecord();
+				}
+
+
+			}
+
+		}
+	}
+}
